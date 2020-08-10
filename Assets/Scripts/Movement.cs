fe98386c7f50e573a0a443b9a527ee38a26a5827
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-08-10 14:18:33 +0800

    Initial Push
    Scripting Survival Guide
    added Movement using Translate and Axis

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(_horizontalInput, 0, 0) * _speed * Time.deltaTime);
+
+        float _verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, 0, _verticalInput) * _speed * Time.deltaTime);
+    }
+}
