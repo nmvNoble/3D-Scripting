dac677c74cf00a4ed9287a53d052f3930714df77
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-08-10 19:09:01 +0800

    Rotations
    Aim Script w/ scene raycast
    RGB Materials
    Smoothed aim using Slerp

---
+++ Assets/Scripts/Aim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    [SerializeField]
+    private int _aimSpeed = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //direction to face = destination - source
+        Vector3 toFace = _target.position - transform.position;
+        Debug.DrawRay(transform.position, toFace, Color.red);
+
+        //access current rotation = quaternion look rotation
+        Quaternion targetRotation = Quaternion.LookRotation(toFace);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime*_aimSpeed);
+    }
+}
