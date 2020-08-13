d310ae2b753bfbefd9e02af89e8f93f0be1f2194
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-08-13 19:51:15 +0800

    Classes
    renamed Rotation Scene
    Added new Scene for Serialized Custom Classes
    Wizard & Spell Script implementing dynamic spells based on Lvl
    Consumable & Weapon Script for inheritance of Item Script
    Pet & Duck Script implements protected and virtual/override functions

---
+++ Assets/Scripts/Unity C# Survival Guide/Non-mono Classes/Consumable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Consumable : Item
+{
+    public int hpEffect;
+    public string statusName;
+    public bool statusEffect;
+
+    public Consumable(string name, int id, string description, 
+            int hpEffect, string statusName, bool statusEffect)
+            :base(name, id, description)
+    {
+        this.name = name;
+        this.id = id;
+        this.description = description;
+        this.hpEffect = hpEffect;
+        this.statusName = statusName;
+        this.statusEffect = statusEffect;
+    }
+}
---
+++ Assets/Scripts/Unity C# Survival Guide/Non-mono Classes/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public int id;
+    public string description;
+    public Sprite icon;
+
+    public Item(string name, int id, string desc)
+    {
+        this.name = name;
+        this.id = id;
+        this.description = desc;
+    }
+}
