38a0ce2289133b393f974ede99af050222e20b39
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-08-14 14:34:48 +0800

    Static Types
    Enemy Prefab
    Spawn Manager, Enemy, & UIManager Scripts for static type enemy counter
    UtilityHelper Static Class for general quick function calls used in Bot
    organized scripts and scenes

---
+++ Assets/Scripts/Unity C# Survival Guide/3 - Intermediate pt1/Non-mono Classes/Spell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Spell
+{
+    public string name;
+    public int lvlRequired;
+    public int expGained;
+
+    public Spell(string name, int lvlRequired, int expGained)
+    {
+        this.name = name;
+        this.lvlRequired = lvlRequired;
+        this.expGained = expGained;
+    }
+
+    public int Cast()
+    {
+        Debug.Log("Casting: " + this.name);
+        return this.expGained;
+    }
+}
