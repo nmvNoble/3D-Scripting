2d507d435cc22d3235abfe7f3d20c68066231797
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2021-01-11 16:20:35 +0800

    Scene Organization & Pause
    Removed Event Objects and Command Pattern for now
    Added Pause Button and edited Pause Script

--- Assets/Scripts/Unity C# Survival Guide/1 - Basics/Pause.cs
+++ Assets/Scripts/Unity C# Survival Guide/1 - Basics/Pause.cs
@@ -1,25 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
+    [SerializeField] private Text PauseBtn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 1)
+        if (Input.GetKeyDown(KeyCode.Escape))
+            ToggleTime();
+    }
+
+    public void ToggleTime()
+    {
+        if (Time.timeScale == 1)
         {
             Time.timeScale = 0;
+            PauseBtn.text = "Resume";
         }
-        else if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 0)
+        else if (Time.timeScale == 0)
         {
             Time.timeScale = 1;
+            PauseBtn.text = "Pause";
         }
     }
 }
